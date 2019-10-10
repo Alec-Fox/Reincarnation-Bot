870df4c905d448d8633623c1a34338434a493ebe
Alec <Alecfox8@gmail.com>
2019-10-10 07:18:00 -0400

    added error handling for mute duration

--- src/commands/mute.js
+++ src/commands/mute.js
@@ -4,17 +4,18 @@ const {
 const {
 	MOD_ROLE_ID,
 } = require('../util/constants.js');
+const ms = require('ms');
 module.exports = {
 	name: 'mute',
 	description: 'Mutes a member [Mod use only]',
-	usage: '[@member] [duration] [reason] \n [duration] format: `[[number][s/m/h/d/w/y]]`\n `s`: secs, `m`: mins, `h`:hrs, `d`:days, `w`:weeks, `y`:years',
+	usage: '[@member] [duration] [reason] \n\n `[duration]` = [[number][s/m/h/d/w/y]]\n s: secs, m: mins, h:hrs, d:days, w:weeks, y:years \n Example: .mute @Bot 5h Spam',
 	args: true,
 	cooldown: 5,
 	execute(message, args) {
 		message.delete();
 		if (message.member.highestRole.comparePositionTo(message.guild.roles.find(role => role.id === MOD_ROLE_ID)) < 0) return message.reply('You are not authorized to use this command.');
 		const specifiedMember = message.mentions.members.first();
-		if (!specifiedMember || args[0] != specifiedMember) return message.reply(`You did not submit a valid member to mute.\nThe proper usage would be: \`${message.client.config.prefix}${this.name} ${this.usage}\``);
+		if (!specifiedMember || args[0] != specifiedMember) return message.reply(`You did not submit a valid member to mute.\nThe proper usage would be: \`${message.client.config.prefix}${this.name}\` ${this.usage}`);
 		if (args.length < 3) {
 			const embed = constructEmbed(`\nInvalid command structure.\nThe proper usage would be: \`${message.client.config.prefix}${this.name} ${this.usage}\``, '', null, null);
 			return message.channel.send(embed);
@@ -23,6 +24,10 @@ module.exports = {
 		const reasonsArray = args.splice(2, args.length);
 		let reasons = reasonsArray.toString();
 		reasons = reasons.replace(/,/g, ' ');
+		if(ms(args[1]) === undefined) {
+			const embed = constructEmbed(`Invalid Duration. \nThe proper usage would be: \`${message.client.config.prefix}${this.name} ${this.usage}\``);
+			return message.send(embed);
+		}
 		message.client.memberinfo[specifiedMember.id].mute(message, specifiedMember, args[1], reasons);
 	},
 };
